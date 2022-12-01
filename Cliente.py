214c1d4414118c4bacb6b4851ad0249f96ea9044
GabIkejima <gabrielhigaikejima@gmail.com>
2022-12-01 18:23:20 -0300

    Class para adicionar clientes

---
+++ Cliente.py
@@ -0,0 +1,6 @@
+class Cliente:
+    def __init__(self, nome, idade, tel):
+        
+        self.nome = nome
+        self.idade = idade
+        self.tel = tel
--- Main.py
+++ Main.py
@@ -1,2 +1,8 @@
 class Main:
     pass
+
+from Cliente import Cliente
+
+cliente_1 = Cliente("Gabriel", "21", "1194444-2222")
+
+print("Cliente:", cliente_1.nome, "\nIdade:", cliente_1.idade, "\nTelefone:", cliente_1.tel)
