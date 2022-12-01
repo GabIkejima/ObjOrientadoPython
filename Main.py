dae9fa4e7159f35a4a63cde40c0aaf0251dbbcc6
GabIkejima <gabrielhigaikejima@gmail.com>
2022-12-01 18:42:48 -0300

    Adiciona e importa conta na main

--- Main.py
+++ Main.py
@@ -2,7 +2,11 @@ class Main:
     pass
 
 from Cliente import Cliente
+from Conta import Conta
+
 
 cliente_1 = Cliente("Gabriel", "21", "1194444-2222")
+conta_1 = Conta(cliente_1.nome, 0000, 0000)
 
-print("Cliente:", cliente_1.nome, "\nIdade:", cliente_1.idade, "\nTelefone:", cliente_1.tel)
+#print("Cliente:", cliente_1.nome, "\nIdade:", cliente_1.idade, "\nTelefone:", cliente_1.tel)
+print("Titular:",conta_1.titular, "\nID:", conta_1.id ,"\nSaldo:", conta_1.saldo)
